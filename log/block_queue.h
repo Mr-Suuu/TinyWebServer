5c4754e38b9c785f872aa21bfffd8f45b7068b4a
sjm <742019617@qq.com>
2023-05-07 21:02:54 +0800

    2023-05-07 09 日志系统（上）

--- log/block_queue.h
+++ log/block_queue.h
@@ -14,14 +14,17 @@
 
 using namespace std;
 
+// 当队列为空时，从队列中获取元素的线程将会被挂起；当队列是满时，往队列里添加元素的线程将会挂起
 template<class T>
 class block_queue {
 public:
+    // 初始化私有成员
     block_queue(int max_size = 1000) {
         if (max_size <= 0) {
             exit(-1);
         }
 
+        // 构造函数创建循环数组
         m_max_size = max_size;
         m_array = new T[max_size];
         m_size = 0;
@@ -45,7 +48,7 @@ public:
         m_mutex.unlock();
     }
 
-    //判断队列是否满了
+    // 判断队列是否满了
     bool full() {
         m_mutex.lock();
         if (m_size >= m_max_size) {
@@ -57,7 +60,7 @@ public:
         return false;
     }
 
-    //判断队列是否为空
+    // 判断队列是否为空
     bool empty() {
         m_mutex.lock();
         if (0 == m_size) {
@@ -68,7 +71,7 @@ public:
         return false;
     }
 
-    //返回队首元素
+    // 返回队首元素
     bool front(T &value) {
         m_mutex.lock();
         if (0 == m_size) {
@@ -80,7 +83,7 @@ public:
         return true;
     }
 
-    //返回队尾元素
+    // 返回队尾元素
     bool back(T &value) {
         m_mutex.lock();
         if (0 == m_size) {
@@ -112,41 +115,49 @@ public:
         return tmp;
     }
 
-    //往队列添加元素，需要将所有使用队列的线程先唤醒
-    //当有元素push进队列,相当于生产者生产了一个元素
-    //若当前没有线程等待条件变量,则唤醒无意义
+    // 往队列添加元素，需要将所有使用队列的线程先唤醒
+    // 当有元素push进队列,相当于生产者生产了一个元素
+    // 若当前没有线程等待条件变量,则唤醒无意义
     bool push(const T &item) {
-
+        // 先上锁
         m_mutex.lock();
         if (m_size >= m_max_size) {
-
+            // 若当前可用个数大于等于最大个数
+            // 则直接广播唤醒所有元素并解锁，返回false
             m_cond.broadcast();
             m_mutex.unlock();
             return false;
         }
 
+        // 将新增数据放在循环数组的对应位置
         m_back = (m_back + 1) % m_max_size;
         m_array[m_back] = item;
-
         m_size++;
 
+        // 唤醒元素
         m_cond.broadcast();
         m_mutex.unlock();
         return true;
     }
 
-    //pop时,如果当前队列没有元素,将会等待条件变量
+    // pop时,如果当前队列没有元素,将会等待条件变量
     bool pop(T &item) {
-
+        // 先上锁
         m_mutex.lock();
+        // 多个消费者的时候，这里应该用while而不是if
         while (m_size <= 0) {
-
+            // 资源不足的情况
+            // 当重新抢到互斥锁，m_cond.wait会返回0
             if (!m_cond.wait(m_mutex.get())) {
+                // 表示发生互斥的情况
+                // 解锁
                 m_mutex.unlock();
                 return false;
             }
         }
 
+        // 当有足够资源的时候
+        // 取出队列首的元素，使用循环数组模拟队列
         m_front = (m_front + 1) % m_max_size;
         item = m_array[m_front];
         m_size--;
@@ -154,7 +165,9 @@ public:
         return true;
     }
 
-    //增加了超时处理
+    // 增加了超时处理（项目中没有使用到）
+    // 取出队列首的元素，这里需要理解一下，使用循环数组模拟的队列
+    // 其他逻辑不变
     bool pop(T &item, int ms_timeout) {
         struct timespec t = {0, 0};
         struct timeval now = {0, 0};
@@ -182,6 +195,7 @@ public:
     }
 
 private:
+    // 创建互斥锁和同步变量
     locker m_mutex;
     cond m_cond;
 
--- log/log.cpp
+++ log/log.cpp
@@ -18,28 +18,35 @@ Log::~Log() {
     }
 }
 
-//异步需要设置阻塞队列的长度，同步不需要设置
+// 异步需要设置阻塞队列的长度，同步不需要设置
 bool Log::init(const char *file_name, int close_log, int log_buf_size, int split_lines, int max_queue_size) {
-    //如果设置了max_queue_size,则设置为异步
+    // 如果设置了max_queue_size,则设置为异步
     if (max_queue_size >= 1) {
+        // 表示异步处理
         m_is_async = true;
+
+        // 创建并设置阻塞队列长度
         m_log_queue = new block_queue<string>(max_queue_size);
         pthread_t tid;
-        //flush_log_thread为回调函数,这里表示创建线程异步写日志
+
+        // flush_log_thread为回调函数,这里表示创建线程异步写日志
         pthread_create(&tid, NULL, flush_log_thread, NULL);
     }
 
     m_close_log = close_log;
+    // 输出内容的长度
     m_log_buf_size = log_buf_size;
     m_buf = new char[m_log_buf_size];
     memset(m_buf, '\0', m_log_buf_size);
+
+    // 日志的最大行数
     m_split_lines = split_lines;
 
     time_t t = time(NULL);
     struct tm *sys_tm = localtime(&t);
     struct tm my_tm = *sys_tm;
 
-
+    // 从后往前找到第一个/的位置
     const char *p = strrchr(file_name, '/');
     char log_full_name[256] = {0};
 
